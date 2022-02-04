2c6cd64a877d0219819c3eb2256bf6575be98e09
mvidaldp <mvidal.dp@gmail.com>
2022-02-04 14:51:56 +0100

    Update liblsl source code from 1.15.0 to 1.15.2

---
+++ app/liblsl/update_lslboost.sh
@@ -0,0 +1,15 @@
+# the absolute path to the extracted boost source archive (https://www.boost.org/users/download/)
+set -e
+set -x
+BOOSTPATH=/tmp/boost_1_75_0
+TMPPATH=/tmp/lslboost
+
+# copy all needed boost files and rename all mentions of boost to lslboost
+mkdir -p $TMPPATH
+bcp --unix-lines --boost=$BOOSTPATH --namespace=lslboost --scan `find src -regex ".+\.[ch]p*"` lslboost/asio_objects.cpp $TMPPATH
+# remove superfluous directories:
+rm $TMPPATH/Jamroot
+find $TMPPATH -type d -and \( -name build -o -name test -o -name edg -o -name dmc -o -name msvc70 -o -name msvc60 -o -name bcc* \) -print0 | xargs -0 rm -rf
+
+rsync -HAXavr --del $TMPPATH/{boost,libs} lslboost
+
